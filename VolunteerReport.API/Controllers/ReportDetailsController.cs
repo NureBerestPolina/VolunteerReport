3319dea27d57fd866339e4fbe38a444133c6c56c
Polina Berest <pamapolya@gmail.com>
2024-11-23 18:32:46 +0200

    Added ReportDatailsController

---
+++ VolunteerReport.API/Controllers/ReportDetailsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VolunteerReport.Domain.Models;
+using VolunteerReport.Domain;
+
+namespace VolunteerReport.API.Controllers
+{
+    public class ReportDetailsController : ODataControllerBase<ReportDetail>
+    {
+        public ReportDetailsController(ApplicationDbContext appDbContext) : base(appDbContext)
+        {
+        }
+    }
+}
